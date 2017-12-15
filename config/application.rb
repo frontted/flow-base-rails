52e4df91a72598e21a0d92816d8c4433ca6d49ab
Adrian Demian <adrian@themefy.io>
2017-12-15 14:09:56 +0200

    add font assets precompile  on application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module FlowBaseRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
