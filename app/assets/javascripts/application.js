57453f30fb04d56d44b7a77be8d136a0ffc7fadf
Adrian Demian <adrian@themefy.io>
2017-12-15 13:45:22 +0200

    add boilerplate from flow pro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,46 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
+//= require jquery/dist/jquery.min.js
+//= require popper.js/dist/umd/popper.js
+//= require bootstrap/dist/js/bootstrap.min.js
+
+//= require dom-factory/dist/dom-factory.js
+//= require material-design-kit/dist/material-design-kit.js
+
+$('[data-toggle="tooltip"]').tooltip({ container: 'body' });
+$('[data-toggle="popover"]').popover();
+
+$('.dropdown.notifications ul a.nav-link').click(function (e) {
+  e.stopPropagation();
+  $(this).tab('show');
+});
+
+// COLOR VARIABLES
+window.colors = {
+  color_warning: "#F5B666",
+  color_success: "#7DC668",
+  color_primary: "#6774DF",
+  color_danger : "#ff7076",
+  color_blue   : "#549DE3",
+  color_bg     : "rgba(239,244,248,1)",
+  border_color : "#E2E8EE",
+  facebook     : '#3b5998',
+  twitter      : '#00aced',
+  instagram    : '#E4405F',
+  dribbble     : '#EA4C89'
+}
+
+
+// Self Initialize DOM Factory Components
+domFactory.handler.autoInit()
+
+// Connect button(s) to drawer(s)
+var sidebarToggle = Array.prototype.slice.call(document.querySelectorAll('[data-toggle="sidebar"]'))
+
+sidebarToggle.forEach(function (toggle) {
+  toggle.addEventListener('click', function (e) {
+    var selector = e.currentTarget.getAttribute('data-target') || '#default-drawer'
+    var drawer = document.querySelector(selector)
+    if (drawer) {
+      drawer.mdkDrawer.toggle()
+    }
+  })
+})
